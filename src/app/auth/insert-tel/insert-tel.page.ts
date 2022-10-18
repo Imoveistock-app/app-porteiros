0ca356e68be19ce8348f0ef52fdd25230bf7abe4
luishdsg <65797541+luishdsg@users.noreply.github.com>
2022-10-18 04:29:51 -0300

    SEND-TEL-PAGE-CREATE

---
+++ src/app/auth/insert-tel/insert-tel.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-tel',
+  templateUrl: './insert-tel.page.html',
+  styleUrls: ['./insert-tel.page.scss'],
+})
+export class InsertTelPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
