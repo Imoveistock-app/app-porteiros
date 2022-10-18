f14492833e66af9518b7552bfb8ab2dbaaf35f4b
luishdsg <65797541+luishdsg@users.noreply.github.com>
2022-10-18 04:30:50 -0300

    LOGIC-SEND-SMS-CREATE

---
+++ src/app/auth/send-tel/send-tel.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-send-tel',
+  templateUrl: './send-tel.page.html',
+  styleUrls: ['./send-tel.page.scss'],
+})
+export class SendTelPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
