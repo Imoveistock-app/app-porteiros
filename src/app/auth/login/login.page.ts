a3a2e4d430f9dee6d46b4def06938727a7ebddc6
luishdsg <65797541+luishdsg@users.noreply.github.com>
2022-10-18 04:30:37 -0300

    CHANGE-DIRETORI-OF-LOGIN-PAGE

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
 export class LoginPage implements OnInit {
 
 
-  btnsSend = true;
-  constructor() { }
 
-  ngOnInit() {
+  form: FormGroup;
+
+  public maskTel: Array<any> = ['(',/\d/, /\d/, ')' ,/\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group({
+      tel: ['', [Validators.required]]
+    });
+  }
+  ngOnInit() {}
+  sendSMS(){
+
+  }
+
+  insertTel(){
+
+  }
+
+  sendTel(){
+
   }
 
 }
