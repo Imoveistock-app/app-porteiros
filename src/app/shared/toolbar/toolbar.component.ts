9b90dfc666eb8ad82bd4f070890e184e09a12a64
luishdsg <65797541+luishdsg@users.noreply.github.com>
2022-10-19 14:02:35 -0300

    ALL-NEW-PAGES-ADD-IN-LOGGED-MODULE

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-toolbar',
