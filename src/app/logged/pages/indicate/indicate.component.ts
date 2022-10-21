9b327779dc1629e9e8fc9529be8ee892a4cdc836
luishdsg <65797541+luishdsg@users.noreply.github.com>
2022-10-21 14:48:01 -0300

    CHANGE SERVICE NAME TO SERVICES

--- src/app/logged/pages/indicate/indicate.component.ts
+++ src/app/logged/pages/indicate/indicate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-indicate',
@@ -6,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./indicate.component.scss'],
 })
 export class IndicateComponent implements OnInit {
+  formIndicate: FormGroup;
 
-  constructor() { }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { 
+    this.formIndicate = this.formBuilder.group({
+      cep: ['', [Validators.required]],
+      road: ['', [Validators.required]],
+      numberResidence: ['', [Validators.required]],
+      complement: ['', [Validators.required]],
+      ownerName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      ownerContact: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit() {}
+
 
 }
