d860e0375f126684ae9f53d7d7a86928b1e3abd8
Danilo de Falco <57508216+danilodefalco@users.noreply.github.com>
2022-11-09 17:56:56 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   payloadKey: '57b0e476-142c-4762-8547-140b65cb1e9c',
   apis: {
     // imoveistock: 'http://ec2-34-201-150-233.compute-1.amazonaws.com:3000/',
